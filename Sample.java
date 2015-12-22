d247fba85e68d7dd997cda9dd9c326c6256bceed
System Administrator <root@chandans-MacBook-Pro.local>
2015-12-22 13:03:26 -0600

    Added Interview Preparation file

---
+++ Sample.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+
+public class Sample {
+
+	
+	public static void main(String[] args) {
+		
+		HashMap<Character, Character>map = new HashMap<Character, Character>();
+		map.put('6', '9');
+		map.put('9', '6');
+		map.put('0', '0');
+		map.put('1', '1');
+		map.put('8', '8');
+		
+		String num = "996";
+		int i=0;
+		int j = num.length()-1;
+		boolean flag = true;
+		char val;
+		while(i<=j){
+			val = map.get(num.charAt(j));
+			if(val!=num.charAt(i)){
+				flag = false;
+				break;
+			}
+			i++;
+			j--;
+			
+		}
+		if(flag){
+			System.out.println("Fancy");
+		}
+		else{
+			System.out.println("Not Fancy");
+		}
+	}
+}
