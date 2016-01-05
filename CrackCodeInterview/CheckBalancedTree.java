7ca0e9d056da8beeb8926cd782ca61d99a20ded8
Chandan Parameswaraiah <cbp698@gmail.com>
2016-01-04 23:28:43 -0600

    Update CheckBalancedTree.java

--- CrackCodeInterview/CheckBalancedTree.java
+++ CrackCodeInterview/CheckBalancedTree.java
@@ -9,7 +9,7 @@ public class CheckBalancedTree {
 	
 	public static void main(String args[]){
 		
-		Node root = new Node(1);
+		Node root = new Node(1); 
 		root.left = new Node(2);
 		root.right = new Node(3);
 		root.left.left = new Node(4);
