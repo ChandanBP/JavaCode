f08eaf4db4eae9c0f09bbc2477b0340ad97403a0
System Administrator <root@chandans-MacBook-Pro.local>
2016-01-04 22:51:49 -0600

    LCA

---
+++ CrackCodeInterview/LeastCommonAncestor.java
@@ -0,0 +1,62 @@
+public class LeastCommonAncestor {
+	private class Node{
+		int key;
+		Node left;
+		Node right;
+		
+		public Node(int k){
+			this.key = k;
+		}
+	}
+	
+	Node root;
+	
+	public void buildTree(){
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.left.left.left = new Node(7);
+		
+		Node parent = findCommonAncestor(root.left.left,root.left.right);
+		if(parent!=null){
+			System.out.println(parent.key);
+		}
+		else{
+			System.out.println("Not present");
+		}
+	}
+	
+	public Node getParent(Node root,Node n1,Node n2){
+		
+		if(root==null)return null;
+		if(root==n1||root==n2)return root;
+		
+		
+		Node left,right;
+		left = getParent(root.left,n1,n2);
+		right = getParent(root.right,n1,n2);
+		
+		if(left!=null&&right!=null)return root;
+		
+	   return (left!=null)?left:right;	
+	}
+	
+	public Node findCommonAncestor(Node n1,Node n2){
+		
+		if(n1==null && n2==null)return null;
+		if(root==null)return null;
+		
+		Node ancestor = getParent(root,n1,n2);
+		return ancestor;
+	}
+	
+		
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Ideone obj = new Ideone();
+		obj.buildTree();
+	}
+}
